871e1a88254a0133c50fbad3f8375e390de6ef50
Cédric Taschereau <cedric.taschereau@gmail.com>
2021-06-06 21:35:35 -0400

    Fix pin setup

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep function from the time module
 
+GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+GPIO.setwarnings(False)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,12 +16,9 @@ def index():
 def control():
 	print(request.json['direction'])
 	print(request.json['event'])
-	GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-	GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.output(8, GPIO.HIGH) # Turn on
 	sleep(1)                  # Sleep for 1 second
 	GPIO.output(8, GPIO.LOW)  # Turn off
-	GPIO.cleanup()
 	return ('', 204)
 
 if __name__ == '__main__':
